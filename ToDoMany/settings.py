b1ec743bb37f7467014ee336f2262084e4ccc135
bezhan2009 <karimovbezan0@gmail.com>
2024-04-02 18:03:59 +0300

    Add: Database ElephantSQL

--- ToDoMany/settings.py
+++ ToDoMany/settings.py
@@ -78,17 +78,19 @@ WSGI_APPLICATION = 'ToDoMany.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'todomaindb',  # Имя базы данных
-        'USER': 'postgres',  # Имя пользователя базы данных
-        'PASSWORD': 'bezhan2009',  # Пароль пользователя базы данных
-    }
-}
 
 """
     DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'todomaindb',  # Имя базы данных
+            'USER': 'postgres',  # Имя пользователя базы данных
+            'PASSWORD': 'bezhan2009',  # Пароль пользователя базы данных
+        }
+    }
+    
+"""
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'fmsehwdu',  # Имя базы данных
@@ -98,8 +100,6 @@ DATABASES = {
             'PORT': '5432',  # Порт базы данных (обычно 5432)
         }
     }
-"""
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
