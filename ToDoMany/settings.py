455917af5c7bf2f0cd21998b5f4125f330063265
bezhan2009 <karimovbezan0@gmail.com>
2024-04-03 11:57:39 +0300

    bugfix

--- ToDoMany/settings.py
+++ ToDoMany/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#2n#&16h+2neo(&n@w6sd&y^mfu(=+n(5mq!rj!a0gu@9$3ol^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', 'bezhan2009.pythonanywhere.com']
 
