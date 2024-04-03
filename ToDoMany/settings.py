0dab5e7cbf5255f9aca702eb0827ffcccbc076e5
bezhan2009 <karimovbezan0@gmail.com>
2024-04-03 11:57:09 +0300

    bugfix

--- ToDoMany/settings.py
+++ ToDoMany/settings.py
@@ -80,8 +80,19 @@ WSGI_APPLICATION = 'ToDoMany.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 """
-    
     DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'todomaindb',  # Имя базы данных
+            'USER': 'postgres',  # Имя пользователя базы данных
+            'PASSWORD': 'bezhan2009',  # Пароль пользователя базы данных
+        }
+    }
+    
+"""
+
+
+DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'fmsehwdu',  # Имя базы данных
@@ -89,15 +100,6 @@ WSGI_APPLICATION = 'ToDoMany.wsgi.application'
             'PASSWORD': 'dQfWrGxmF8vO1S60SxtgQjTKYtSQ8_5j',  # Пароль пользователя базы данных
             'HOST': 'rain.db.elephantsql.com',  # Хост базы данных
             'PORT': '5432',  # Порт базы данных (обычно 5432)
-        }
-    }
-"""
-DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'todomaindb',  # Имя базы данных
-            'USER': 'postgres',  # Имя пользователя базы данных
-            'PASSWORD': 'bezhan2009',  # Пароль пользователя базы данных
         }
     }
 
