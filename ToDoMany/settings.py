5a3364d3d4b5a4e75542803014cae55c4fa15863
bezhan2009 <karimovbezan0@gmail.com>
2024-04-06 12:17:54 +0300

    Add: Connection with front_end

--- ToDoMany/settings.py
+++ ToDoMany/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_yasg',
     'rest_framework_simplejwt',
-    'ToDoSource'
+    'ToDoSource',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'ToDoMany.urls'
@@ -78,7 +80,19 @@ WSGI_APPLICATION = 'ToDoMany.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
 DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todomaindb',  # Имя базы данных
+        'USER': 'postgres',  # Имя пользователя базы данных
+        'PASSWORD': 'bezhan2009',  # Пароль пользователя базы данных
+    }
+}
+
+"""
+
+    DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
             'NAME': 'fmsehwdu',  # Имя базы данных
@@ -86,18 +100,6 @@ DATABASES = {
             'PASSWORD': 'dQfWrGxmF8vO1S60SxtgQjTKYtSQ8_5j',  # Пароль пользователя базы данных
             'HOST': 'rain.db.elephantsql.com',  # Хост базы данных
             'PORT': '5432',  # Порт базы данных (обычно 5432)
-        }
-    }
-
-"""
-
-    
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'todomaindb',  # Имя базы данных
-            'USER': 'postgres',  # Имя пользователя базы данных
-            'PASSWORD': 'bezhan2009',  # Пароль пользователя базы данных
         }
     }
 """
@@ -218,4 +220,7 @@ LOGGING = {
         },
     },
 }
+
+
+CORS_ALLOW_ALL_ORIGINS = True
 
