d71d8892828a2efbc33d8bfb79db02e3aa966471
saijaku0 <minerking112256@gmail.com>
2024-04-09 17:27:39 +0300

    fix

--- frontend/src/pages/errorPage/ErrorPage.jsx
+++ frontend/src/pages/errorPage/ErrorPage.jsx
@@ -8,7 +8,8 @@ export default function ErrorPage({error}) {
       <h1 className="error-title">Oops!</h1>
       <p className="error-text">Sorry, an unexpected error has occurred.</p>
       <p className="error-text">
-        <i>{error.statusText || error.message}</i>
+        {/* <i>{error.statusText || error.message}</i> ЕЩЕ ОДНА ЗАГЛУШКА */}
+        <i>{error || error}</i>
       </p>
     </div>
   );
