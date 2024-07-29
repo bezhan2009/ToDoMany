1d112def11d25bacb8399d98304477b45c822801
AbyssLinTU <marvovolen@gmail.com>
2024-07-29 23:35:53 +0300

    fix 29-07-2024

---
+++ frontend/src/pages/registration.jsx
@@ -0,0 +1,23 @@
+import '../css/registration.css';
+const Regis = () => {
+    return (
+        <div className='html'>
+        <form className="form_regis">
+            <h1>Добро пожаловать</h1>
+            <div className="cont">
+                <div className="input_cont">
+                    <input type="email" placeholder="Логин" className="text-input" id="loginInput"/>
+                    <input type="password" placeholder="Пароль" className="text-input" id="passwordInput1"/>
+                    <input type="password" placeholder="Подтвердите пароль" className="text-input" id="passwordInput2"/>
+                </div>
+                <button className="btnRegister">Регистрация</button>
+                <div className="if_cont">
+                    <p>Есть аккаунт?</p> <a className="login" href="/login">Вход</a>
+                </div>
+            </div>
+        </form>
+        </div>
+    );
+};
+
+export default Regis;
