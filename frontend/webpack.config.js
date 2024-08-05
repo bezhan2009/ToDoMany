35c1e0b0859f9c217ae4487afb7a985b912f0541
Bejan <157696306+bezhan2009@users.noreply.github.com>
2024-08-05 21:34:00 +0300

    Update webpack.config.js

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -2,25 +2,42 @@ const path = require('path');
 const { ProvidePlugin } = require('webpack');
 
 module.exports = {
-    // ... other Webpack config
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env", "@babel/preset-react"]
-                    }
-                }
-            },
-            // ... other loaders
-        ]
-    },
-    plugins: [
-        new ProvidePlugin({
-            React: 'react'
-        })
+  entry: {
+    main: path.resolve(__dirname, './src/index.js'),
+  },
+
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      // Добавьте другие необходимые загрузчики, например для изображений
     ]
+  },
+
+  plugins: [
+    new ProvidePlugin({
+      React: 'react'
+    })
+  ],
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
